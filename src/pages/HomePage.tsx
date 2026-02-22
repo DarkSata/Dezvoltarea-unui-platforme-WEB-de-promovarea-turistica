e36a91dc6b9f498e7c6243a5bc1f41ee7ce6986a
Daniel <idanplay2019@gmail.com>
2026-02-22 16:56:08 +0200

    Add header placeholder links for Ghid/Galerie/Contact and home hash scroll

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { HeroSection } from "../sections/HeroSection";
 import { TopPlacesSection } from "../sections/TopPlacesSection";
 import { RoutesSection } from "../sections/RoutesSection";
 import { GuideSection } from "../sections/GuideSection";
 import { GallerySection } from "../sections/GallerySection";
 import { PerksSection } from "../sections/PerksSection";
+import { scrollToId } from "../utils/scroll";
 
 export default function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const sectionId = location.hash.replace(/^#/, "");
+    if (!sectionId) {
+      return;
+    }
+
+    const frameId = window.requestAnimationFrame(() => {
+      scrollToId(sectionId);
+    });
+
+    return () => {
+      window.cancelAnimationFrame(frameId);
+    };
+  }, [location.hash]);
+
   return (
     <>
       <HeroSection />
